ffd99f9f2bd0497d92859d9035dcf1898ae5e707
DanielTakov <daniel.takov@theopentag.com>
2023-01-20 22:41:34 +0200

    Add project files.

---
+++ Summator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Summator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Summator.Test_SumTwoNumbers();
+        }
+    }
+}
---
+++ Summator/Summator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Summator
+{
+    public static class Summator
+    {
+        public static int Sum(int[] arr)
+        { 
+
+        int sum = arr[0];
+
+        for ( int i = 1; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            return sum;
+        }
+
+        public static void Test_SumTwoNumbers()
+        {
+            if (Sum(new int[] { 1, 3 }) != 3)
+            {
+                throw new Exception("1+2 != 3");
+            }else
+            {
+                Console.WriteLine("Test Pass!");
+            }
+                 
+
+        }
+
+
+
+    }
+}
